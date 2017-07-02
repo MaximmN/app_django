c66fb4f64a2a6dad299347bdffb5d58248a7718d
Max <maxim.nechai@gmail.com>
2017-07-02 18:38:00 +0300

    Add new models, stored as a migration.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,8 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-#change time zome
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,4 +11,4 @@ class Question(models.Model):
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
-	votes = models.IntergerField(default=0)
+	votes = models.IntegerField(default=0)
