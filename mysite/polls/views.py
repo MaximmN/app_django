ff2bb9155c73ff70fcaf7916f31a75db7082c015
Max <maxim.nechai@gmail.com>
2017-07-04 11:11:48 +0300

    Added get_object_or_404 function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
@@ -31,8 +31,5 @@ def vote(request, question_id):
 
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
