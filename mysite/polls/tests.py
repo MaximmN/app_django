1d7ba6b2f982568bbc11295e49e75806cd4000f9
Max <maxim.nechai@gmail.com>
2017-07-04 22:16:26 +0300

    Added tests to check Question pub_date displayed or not

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -25,6 +25,27 @@ class QuestionModelTests(TestCase):
         self.assertIs(future_question.was_published_recently(), False)
 
 
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 def test_was_published_recently_with_old_question(self):
 	"""
 	was_published_recently() returns False for questions whose pub_date
