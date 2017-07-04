527e852a273f2da68816f6b1a9de11084f4feeba
Max <maxim.nechai@gmail.com>
2017-07-04 15:23:42 +0300

    Amend views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,26 +5,30 @@ from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -44,8 +48,3 @@ def vote(request, question_id):
 		# with POST data. This prevents data from being postes twice if a 
 		# user hits the Back button.
 	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
